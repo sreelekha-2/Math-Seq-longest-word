641529a4b489ba9e5025906dbc8706ed6830e8be
sreelekha-2 <89189294+sreelekha-2@users.noreply.github.com>
2022-10-14 13:32:02 +0530

    1 file modified

--- index.js
+++ index.js
@@ -31,30 +31,3 @@ const long = arr.reduce((longWord, currentWord) => {
   }
 }, '');
 console.log(long);
-
-// 2.math sequence
-
-const seq = [5, 10, 15, 20];
-let arithmeticCount = 0;
-let geometricCount = 0;
-
-const fst = seq[0];
-const sec = seq[1];
-const d = sec - fst;
-const r = sec / fst;
-
-for (i = 0; i < seq.length; i++) {
-  if (seq[i + 1] - seq[i] === d) {
-    arithmeticCount++;
-  }
-  if (seq[i + 1] / seq[i] === r) {
-    geometricCount++;
-  }
-}
-if (arithmeticCount === seq.length - 1) {
-  console.log('Arithmetic');
-} else if (geometricCount === seq.length - 1) {
-  console.log('Geometric');
-} else {
-  console.log('-1');
-}
